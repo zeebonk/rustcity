58492e4c0fe47e3b01425094e7100bc09ea5f4ac
Gijs van der Voort <vandervoort.gijs@gmail.com>
2020-07-26 17:58:43 +0200

    Minor adjustments

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,7 @@ use piston::window::WindowSettings;
 pub struct App {
     gl: GlGraphics,
     rotation: f64,
-    left_down: bool,
-    offset: [f64; 2],
+    camera_offset: [f64; 2],
 }
 
 impl App {
@@ -27,18 +26,17 @@ impl App {
         const FOREGROUND: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
 
         let square = rectangle::square(0.0, 0.0, 300.0);
+        let (square_x, square_y) = (300., 300.);
         let rotation = self.rotation;
-        let (x, y) = (args.window_size[0] / 2.0, args.window_size[1] / 2.0);
-
-        let (dx, dy) = (self.offset[0], self.offset[1]);
+        let (camera_x, camera_y) = (self.camera_offset[0], self.camera_offset[1]);
 
         self.gl.draw(args.viewport(), |c, gl| {
             clear(BACKGROUND, gl);
 
-            let transform = c.transform.trans(-dx, -dy);
+            let transform = c.transform.trans(-camera_x, -camera_y);
 
             let square_transform = transform
-                .trans(x, y)
+                .trans(square_x, square_y)
                 .rot_rad(rotation)
                 .trans(-150.0, -150.0);
 
@@ -47,7 +45,6 @@ impl App {
     }
 
     fn update(&mut self, args: &UpdateArgs) {
-        // Rotate 2 radians per second.
         self.rotation += 2.0 * args.dt;
     }
 }
@@ -67,9 +64,10 @@ fn main() {
     let mut app = App {
         gl: GlGraphics::new(opengl),
         rotation: 0.0,
-        left_down: false,
-        offset: [-100., -100.],
+        camera_offset: [0., 0.],
     };
+
+    let mut left_down = false;
 
     let mut events = Events::new(EventSettings::new());
     while let Some(e) = events.next(&mut window) {
@@ -82,20 +80,23 @@ fn main() {
         }
 
         if let Some(args) = e.mouse_relative_args() {
-            if app.left_down {
-                app.offset = [app.offset[0] - args[0], app.offset[1] - args[1]];
+            if left_down {
+                app.camera_offset = [
+                    app.camera_offset[0] - args[0],
+                    app.camera_offset[1] - args[1],
+                ];
             }
         }
 
         if let Some(Button::Mouse(b)) = e.press_args() {
             if b == MouseButton::Left {
-                app.left_down = true;
+                left_down = true;
             }
         }
 
         if let Some(Button::Mouse(b)) = e.release_args() {
             if b == MouseButton::Left {
-                app.left_down = false;
+                left_down = false;
             }
         }
     }
