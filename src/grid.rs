7c73ed95701eba7de528a7d80fa8a9883624a04b
Gijs van der Voort <vandervoort.gijs@gmail.com>
2020-07-29 11:25:00 +0200

    Use 1d vec for fields

--- src/grid.rs
+++ src/grid.rs
@@ -1,20 +1,18 @@
 use rand::{thread_rng, Rng};
 
 pub struct Grid {
-    fields: [[i32; u8::MAX as usize]; u8::MAX as usize],
-    width: u8,
-    height: u8,
+    fields: Vec<i32>,
+    width: usize,
+    height: usize,
 }
 
 impl Grid {
-    pub fn new(width: u8, height: u8) -> Grid {
-        let mut fields = [[0; u8::MAX as usize]; u8::MAX as usize];
+    pub fn new(width: usize, height: usize) -> Grid {
+        let mut fields = vec![0; width * height];
 
         let mut random = thread_rng();
-        for y in 0..fields.len() {
-            for x in 0..fields[y].len() {
-                fields[y][x] = random.gen_range(0, 3);
-            }
+        for i in 0..fields.len() {
+            fields[i] = random.gen_range(0, 3);
         }
 
         Grid {
@@ -24,15 +22,15 @@ impl Grid {
         }
     }
 
-    pub fn get(&self, x: u8, y: u8) -> i32 {
-        self.fields[y as usize][x as usize]
+    pub fn get(&self, x: usize, y: usize) -> i32 {
+        self.fields[x + self.width * y]
     }
 
-    pub fn width(&self) -> u8 {
+    pub fn width(&self) -> usize {
         self.width
     }
 
-    pub fn height(&self) -> u8 {
+    pub fn height(&self) -> usize {
         self.height
     }
 }
